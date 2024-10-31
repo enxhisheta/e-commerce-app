41f7f70ed195d7db4c9fa452d23429429db3f192
Enxhi Sheta <enxhisheta@gmail.com>
2024-10-31 23:44:38 +0100

    fixed unnecesary changes

--- src/UI/CartButton.jsx
+++ src/UI/CartButton.jsx
@@ -1,22 +1,20 @@
 import { useNavigate } from "react-router-dom";
 import { FiShoppingCart } from "react-icons/fi";
-import { useCart } from "../store/CartContext";
+//import { useCart } from "../store/CartContext";
 
 const CartButton = () => {
   const navigate = useNavigate();
-  const { cart } = useCart();
+  //const { cart } = useCart();
 
   const handleCartClick = () => {
     navigate("/cart");
   };
 
-  const itemCount = cart.reduce((sum, item) => sum + item.quantity, 0);
+  // const itemCount = cart.reduce((sum, item) => sum + item.quantity, 0);
 
   return (
     <button onClick={handleCartClick}>
-      <FiShoppingCart size={24}>
-        <span>{itemCount}</span>
-      </FiShoppingCart>
+      <FiShoppingCart size={24}></FiShoppingCart>
     </button>
   );
 };
