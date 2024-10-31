986f0711359b7c17887f08f6ac1d540ab4f29266
Enxhi Sheta <enxhisheta@gmail.com>
2024-10-31 22:10:06 +0100

    refactored ThemeContext

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -4,24 +4,18 @@ import { createContext, useState, useEffect } from "react";
 const ThemeContext = createContext();
 
 const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState("light");
+  const savedTheme = localStorage.getItem("theme") || "light";
+  const [theme, setTheme] = useState(savedTheme);
 
   const toggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
   };
 
   useEffect(() => {
     document.body.setAttribute("data-theme", theme);
-
-    localStorage.setItem("theme", theme);
   }, [theme]);
-
-  useEffect(() => {
-    const savedTheme = localStorage.getItem("theme");
-    if (savedTheme) {
-      setTheme(savedTheme);
-    }
-  }, []);
 
   return (
     <ThemeContext.Provider value={{ darkTheme: theme === "dark", toggleTheme }}>
